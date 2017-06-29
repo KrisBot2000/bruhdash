b3da8c47567001f6c12ff9239b1026c47770528a
Kristin Treder <stuffs@Kristins-MacBook.local>
2017-06-28 20:01:23 -1000

    30 tests passed 14 failures

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -10,97 +10,190 @@ var global = window || GLOBAL;
 global.bruhdash = {
 
   // returns the first element of an array
-  first: function () {
-      
+  first: function (arr) {
+      return arr[0];
   },
 
   // returns the last element of an array
-  last: function () {
+  last: function (arr) {
+      return arr[arr.length-1];
 
   },
 
   // returns the index of the first matching element from left to right
-  indexOf: function () {
-
+  indexOf: function (arr, value) {
+    for(var i=0; i<arr.length; i++){
+      if(arr[i]===value){
+        return i;
+      }
+    }
+    return -1;
   },
 
   // returns the index of the first matching element from left to right
-  lastIndexof: function () {
-
+  lastIndexOf: function (arr, value) {
+    for (var i=(arr.length-1); i>=0; i--){
+      if(arr[i]===value){
+        return i;
+      }
+    }
+    return -1;
   },
 
   // returns an array with all elements except for the last element
-  initial: function () {
-
+  initial: function (arr) {
+    arr.pop();
+    return arr;
   },
-  
-  // returns an array with all falsey values removed
-  compact: function() {
 
+  // returns an array with all falsey values removed
+  compact: function(arr) {
+    let newArr = [];
+    for (var i=0; i<arr.length; i++)
+      if(arr[i]){
+        newArr.push(arr[i]);
+      }
+    return newArr;
   },
 
   // creates a slice of an array from the start index up to but not including the end index
-  slice: function () {
-
+  slice: function (arr, start, end) {
+    let newArr = [];
+    for(i=start; i<end; i++){
+      newArr.push(arr[i]);
+    }
+    return newArr;
   },
 
   // returns a slice of array with n elements dropped from the beignning
-  drop: function(){
-
+  drop: function(arr, n){
+    if(n===undefined){
+      n = 1;
+    }
+    let newArr = [];
+    for(i=n; i<arr.length; i++){
+      newArr.push(arr[i]);
+    }
+    return newArr;
   },
 
   // returns a slice of array with n elements dropped from the end
-  dropRight: function() {
-
+  dropRight: function(arr, n) {
+    if(n===undefined){
+      n = 1;
+    }
+    let newArr = [];
+    for(i=0; i<(arr.length - n); i++){
+      newArr.push(arr[i]);
+    }
+    return newArr;
   },
 
   // creates a slice of an array with n elements taken from the beginning
-  take: function () {
-
+  take: function (arr, n) {
+    if(n===undefined){
+      n = 1;
+    }
+    if(n < arr.length){
+      let newArr = [];
+      for(i=0; i<n; i++){
+        newArr.push(arr[i]);
+      }
+      return newArr;
+    }else{
+      return arr;
+    }
   },
 
   // creates a slice of an array with n elements taken from the end
-  takeRight: function () {
-
+  takeRight: function (arr, n) {
+    if(n===undefined){
+      n = 1;
+    }
+    let newArr = [];
+    for(i=(arr.length - n); i<arr.length; i++){
+      newArr.push(arr[i]);
+    }
+    return newArr;
   },
 
   // fills elements of array with specified value from the start index
   // up to but not including the end index
-  fill: function() {
-
+  fill: function(arr, value, start, end) {
+    if(start===undefined || end===undefined){
+      start = 0;
+      end = arr.length;
+    }
+    for(i=start; i<end; i++){
+      arr[i] = value;
+    }
+    return arr;
   },
 
   // removes all given values from an array
-  pull: function () {
-
+  pull: function (arr, value) {
+    for(j=0; j<value.length; j++){
+      for (var i=(arr.length -1); i>=0; i--){
+        if(arr[i]===value[j]){
+          arr.splice(i, 1);
+        }
+      }
+    }
+    return arr;
   },
 
   // removes elements of an array corresponding to the given indices
-  pullAt: function () {
-
+  pullAt: function (arr, indices) {
+    for(i=indices.length-1; i>=0; i--){
+      arr.splice(indices[i],1);
+    }
+    return arr;
   },
 
   // creates an array excluding all the specified values
-  without: function() {
-
+  without: function(arr, values) {
+    for(i=arr.length-1; i>=0; i--){
+      for(j=values.length-1; j>=0; j--){
+        if(arr[i]===values[j]){
+          arr.splice(i,1);
+        }
+      }
+    }
+    return arr;
   },
 
   // returns an array with specified values excluded
-  difference: function() {
-
+  difference: function(arr1, arr2) {
+    for(i=arr1.length-1; i>=0; i--){
+      for(j=arr2.length-1; j>=0; j--){
+        if(arr1[i]===arr2[j]){
+          arr1.splice(i,1);
+        }
+      }
+    }
+    return arr1;
   },
 
   /*******************
    *  STRETCH GOALS! *
-   *******************/ 
+   *******************/
 
   // creates an array of grouped elements
-  zip: function () {
-
+  zip: function (arr1, arr2) {
+    newArr = [];
+    for(i=0; i<arr1.length; i++){
+      tempArr = [];
+      tempArr.push(arr1[i]);
+      tempArr.push(arr2[i]);
+      newArr.push(tempArr);
+    }
+    return newArr;
   },
 
   // creates an array of grouped elements in their pre-zip configuration
-  unzip: function () {
+  unzip: function (arr1) {
+    console.log(arr1);
+
 
   },
 
@@ -123,7 +216,7 @@ global.bruhdash = {
 
   /*************************
    *  SUPER STRETCH GOALS!  *
-   *************************/ 
+   *************************/
 
   // iterates over elements of a collection and returns all elements that the predicate returns truthy for
   // Note: this should work for arrays and objects
@@ -135,6 +228,6 @@ global.bruhdash = {
   // in the collection through an iteratee
   // Note: this should work for arrays and objects
   reduce: function() {
-    
+
   }
 };
